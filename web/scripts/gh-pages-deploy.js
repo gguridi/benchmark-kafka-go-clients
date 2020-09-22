c4e332c46b10f80429b65663b6487a1b343550d1
Gorka Guridi <gorka.guridi@gmail.com>
2020-09-22 17:34:21 +0100

    No need to go back to master branch.

--- web/scripts/gh-pages-deploy.js
+++ web/scripts/gh-pages-deploy.js
@@ -15,8 +15,6 @@ const fs = require("fs");
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
         await execa("rm", ["-r", folder]);
-        await execa("git", ["checkout", "-f", "master"]);
-        await execa("git", ["branch", "-D", "gh-pages"]);
         console.log("Successfully deployed, check your settings");
     } catch (e) {
         console.log(e.message);
