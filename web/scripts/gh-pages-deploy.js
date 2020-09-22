f568be4ac5c4e2d0f859418b6aaa935eb8ea96a1
Gorka Guridi <gorka.guridi@gmail.com>
2020-09-22 17:27:15 +0100

    Adding publish credentials.

--- web/scripts/gh-pages-deploy.js
+++ web/scripts/gh-pages-deploy.js
@@ -14,7 +14,7 @@ const fs = require("fs");
         await execa("git", ["--work-tree", folder, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-        await execa("rm", ["-r", folderName]);
+        await execa("rm", ["-r", folder]);
         await execa("git", ["checkout", "-f", "master"]);
         await execa("git", ["branch", "-D", "gh-pages"]);
         console.log("Successfully deployed, check your settings");
