1ebeb723a1dd396a7d20133abe61ad4cf5520a80
Gorka Guridi <gorka.guridi@gmail.com>
2020-09-22 20:33:39 +0100

    Adding the rest of the reports.

--- web/src/pages/Producers.vue
+++ web/src/pages/Producers.vue
@@ -38,6 +38,38 @@
             </div>
             <div
                 class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+            ></div>
+        </div>
+
+        <div class="md-layout">
+            <div
+                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+            >
+                <benchmark-chart :type="'producer'" :num="100" :size="5000" />
+            </div>
+            <div
+                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+            >
+                <benchmark-chart :type="'producer'" :num="1000" :size="5000" />
+            </div>
+            <div
+                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+            >
+                <benchmark-chart :type="'producer'" :num="10000" :size="5000" />
+            </div>
+        </div>
+        <div class="md-layout">
+            <div
+                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+            >
+                <benchmark-chart
+                    :type="'producer'"
+                    :num="100000"
+                    :size="5000"
+                />
+            </div>
+            <div
+                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
             >
                 <benchmark-chart
                     :type="'producer'"
@@ -45,6 +77,9 @@
                     :size="5000"
                 />
             </div>
+            <div
+                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+            ></div>
         </div>
     </div>
 </template>
