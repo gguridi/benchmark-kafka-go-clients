4f0c84a91d2f36289c7cdab523ebf83587c568a3
Gorka Guridi <gorka.guridi@gmail.com>
2020-09-25 22:30:39 +0100

    Testing things.

--- main_test.go
+++ main_test.go
@@ -67,6 +67,7 @@ var _ = Describe("Benchmarks", func() {
 				process()
 				<-sarama.Done
 				producer.Close()
+				log.Infof("Finished prepopulating Kafka")
 			}
 			initialised = true
 		})
@@ -114,6 +115,6 @@ var _ = Describe("Benchmarks", func() {
 			default:
 				log.Panicf("Unable to find the libray %+v", Library)
 			}
-		}, 2)
+		}, 1)
 	})
 })
